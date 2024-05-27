f53a808eeb774de999ff692a2a7ad822a3e01280
martins624 <martinstega1@gmail.com>
2024-05-27 02:38:46 +0100

    First Commit

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
-console.log(bot)
-const web_app_link = "https://thedogtapcoin-1a304.web.app";
+const web_app_link = "https://dogtapcoin-188ff.web.app";
 // bot.start((ctx) => ctx.reply('Welcome to Toenail Coin', {reply_markup:{ keyboard: [[{ text: "Toenail Coin Web App", web_app: { url: web_app_link }}]],}}));
 bot.start((ctx) => ctx.reply("Welcome to DogTap.\nTap, tap and tap.\nThere may be a reward at the end. Who knows? 🐶", {reply_markup:{ inline_keyboard: [[{ text: "Play DogTap 🐶", web_app: { url: web_app_link }},]],}}));
 
